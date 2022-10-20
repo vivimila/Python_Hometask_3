5c18d4a8549a9ecc34c5f2362578c9c3a6fcabba
vivimila <violamilashevskaya@yandex.by>
2022-10-20 12:20:04 +0300

    task 2 is solved

--- Task_2.py
+++ Task_2.py
@@ -3,3 +3,13 @@
 #*Пример:*
 # [2, 3, 4, 5, 6] => [12, 15, 16];
 # [2, 3, 5, 6] => [12, 15]
+
+
+listMassive = list(map(int, input("Введите числа (через пробел):\n").split()))
+#listMassive = [2, 3, 5, 6]
+
+сondition = len(listMassive)//2 + 1 if len(listMassive) % 2 != 0 else len(listMassive)//2
+new_list = [listMassive[i]*listMassive[len(listMassive)-i-1] for i in range(сondition)]
+
+#ans = listMassive[::len(listMassive)-1]
+print(new_list)
